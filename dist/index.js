a6a3a03319c47dc2db9a5062265c71e85ff055cf
Israel Abazie <101069893+vintage-creator@users.noreply.github.com>
2023-08-24 18:50:15 +0100

    Create index.js

---
+++ dist/index.js
@@ -0,0 +1,25 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "ThemeProvider", {
+  enumerable: true,
+  get: function get() {
+    return _ThemeContext.ThemeProvider;
+  }
+});
+Object.defineProperty(exports, "ToggleSwitch", {
+  enumerable: true,
+  get: function get() {
+    return _ToggleSwitch.ToggleSwitch;
+  }
+});
+Object.defineProperty(exports, "useTheme", {
+  enumerable: true,
+  get: function get() {
+    return _ThemeContext.useTheme;
+  }
+});
+var _ToggleSwitch = require("./components/ToggleSwitch");
+var _ThemeContext = require("./components/ThemeContext");
